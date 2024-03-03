7c40b5bcdd95b5eb5f37cbbf77bcdcc00af32f14
Erisa A <erisa@erisa.uk>
2024-03-03 01:43:09 +0000

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 // html.d.ts tells typescript that this is a normal thing to do
-import htmlString from './index.html'
+import htmlString from './index.html';
 
 export default {
 	async fetch(req: Request, ctx: ExecutionContext): Promise<Response> {
 		return new Response(htmlString, {
 			headers: {
-				"content-type": "text/html",
-				"cache-control": "public, max-age=7200"
-			}
+				'content-type': 'text/html',
+				'cache-control': 'public, max-age=7200',
+			},
 		});
 	},
 };
